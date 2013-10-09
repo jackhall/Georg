d9501551d1913d5d199ecec86960e4cf14724a4a
jackhall <jackwhall7@gmail.com>
2013-10-09 03:07:43 -0500

    ported to ezxdisp, but there's a font problem

--- draw.scm
+++ draw.scm
@@ -1,49 +1,130 @@
 #!/usr/bin/csi -s
 
-(use simple-graphics)
+; ; ; ezxdisp version
+(use ezxdisp)
 (include "mathh-constants")
 
-(define rule-unit (sqrt 10))
-(define ^
-  (lambda (x n)
-    (if (= n 1) x
-        (^ (* x x) (- n 1)))))
+(define difference
+  (lambda (A B)
+    (cons
+      (- (car B) (car A)) 
+      (- (cdr B) (cdr A)))))
+
+(define rotate-and-scale-down
+  (lambda (A B theta factor)
+    (let ((ab (difference A B))) 
+      (cons 
+        (+ 
+          (* factor
+             (-
+               (* (car ab) (cos theta))
+               (* (cdr ab) (sin theta))))
+          (car A))
+        (+ 
+          (* factor
+             (+
+               (* (car ab) (sin theta))
+               (* (cdr ab) (cos theta))))
+          (cdr A))))))
+
+(define rotate
+  (lambda (A B theta)
+    (let ((ab (difference A B))) 
+      (cons 
+        (+ 
+          (* (car ab) (cos theta))
+          (- (* (cdr ab) (sin theta)))
+          (car A))
+        (+ 
+          (* (car ab) (sin theta))
+          (* (cdr ab) (cos theta))
+          (cdr A))))))
 
 (define rule
-  (lambda (step)
-    (lambda ()
-      (slower rule-unit)
-      (step)
-      (adventure
-        (right)
-        (step)
-        (left)
-        (step))
-      (step)
-      (right)
-      (step)
-      (left)
-      (step)
-      (faster rule-unit))))
+  (lambda (draw-unit)
+    (let ((rule-unit (sqrt 10))
+          (rule-angle (atan (/ 3))))
+      (lambda (A B)
+        (let* ((a (rotate-and-scale-down A B rule-angle rule-unit))
+               (b (rotate-and-scale-down A B (- rule-angle PI/4) rule-unit)))
+               (c (rotate a b PI/2))
+               (d (rotate b a (- PI/2)))
+            (draw-unit A a)
+            (draw-unit a b)
+            (draw-unit a c)
+            (draw-unit b d)
+            (draw-unit c d)
+            (draw-unit d B))))))
 
-(define single-step (lambda () (forward 1)))
+(define image 
+  (ezx-init 640 480 "fractal"))
+(ezx-set-background image (make-ezx-color 1 1 1))
+(define color
+  (make-ezx-color 0 0 0))
 
-(define draw-fractal 
-  (lambda (depth)
-    (clear)
-    (go-to -300 150)
-    (right)
-    (left (/ (* depth (atan (/ 1 3)) 180) PI))
-    (faster (* rule-unit depth 4))
+(define draw-segment
+  (lambda (A B)
+    (ezx-line-2d image (car A) (cdr A) (car B) (cdr B) color)))
+
+(define draw-fractal
+  (lambda (depth start end)
     ((let layer ((depth depth))
-       (if (= depth 0) 
-           (rule single-step)
-           (rule (layer (- depth 1))))))))
+       (if (= depth 0)
+         (rule draw-segment)
+         (rule (layer (- depth 1))))) 
+     start end)))
 
-(draw-fractal 5)
-(save)
+(draw-fractal 2 (cons 10 240) (cons 630 240))
+(ezx-redraw image)
+(ezx-next-event image) ; wait for user to close window
+(ezx-quit image)
 
 
+; ; ; simple-graphics version
+;(use simple-graphics)
+;(include "mathh-constants")
+;
+;(define rule-unit (sqrt 10))
+;(define ^
+;  (lambda (x n)
+;    (if (= n 1) x
+;        (^ (* x x) (- n 1)))))
+;
+;(define rule
+;  (lambda (step)
+;    (lambda ()
+;      (slower rule-unit)
+;      (step)
+;      (adventure
+;        (right)
+;        (step)
+;        (left)
+;        (step))
+;      (step)
+;      (right)
+;      (step)
+;      (left)
+;      (step)
+;      (faster rule-unit))))
+;
+;(define single-step (lambda () (forward 1)))
+;
+;(define draw-fractal 
+;  (lambda (depth)
+;    (clear)
+;    (go-to -300 150)
+;    (right)
+;    (left (/ (* depth (atan (/ 1 3)) 180) PI))
+;    (faster (* rule-unit depth 4))
+;    ((let layer ((depth depth))
+;       (if (= depth 0) 
+;           (rule single-step)
+;           (rule (layer (- depth 1))))))))
+;
+;(draw-fractal 5)
+;(save)
+
+; ; ; cairo version
 ;; extract the last point from a shape
 ;(define shape-end
 ;  (lambda (shape)
